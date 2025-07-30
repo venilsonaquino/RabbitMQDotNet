f65cd5921ca5403d37b60a49d4f0e5b683c16506
venilson2 <venilson2009@hotmail.com>
2025-07-30 20:02:27 -0300

    Add project files for RabbitMQ connection implementation

---
+++ src/01-connection/Program.cs
@@ -0,0 +1,15 @@
+﻿using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName = "localhost", UserName = "admin", Password = "admin", Port = 5672 };
+
+using var connection = await factory.CreateConnectionAsync();
+Console.WriteLine("Conectado ao RabbitMQ com sucesso!");
+using var channel = await connection.CreateChannelAsync();
+Console.WriteLine("Canal criado com sucesso!");
+
+await Task.Delay(30000);
+
+await connection.CloseAsync();
+await channel.CloseAsync();
+
+Console.WriteLine("Conexão fechada com sucesso!");
